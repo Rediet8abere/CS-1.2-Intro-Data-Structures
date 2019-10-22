4202094ce5fb8f68c0ce55dedaf343c7d1cd347d
Rediet8abere <redietaberegesesse@gmail.com>
2019-10-22 10:55:45 -0700

    rearrage code simplified

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,31 +1,23 @@
 import random
 import sys
 
-def rearrange(word_1, word_2, word_3, word_4):
-    word_list = [word_1, word_2, word_3, word_4]
-
-    arrange = len(word_list)-1
-
+def rearrange(words):
+    arrange = len(words)
     while arrange > 0:
-        for i in range(arrange-1):
+        for i in range(arrange):
             index = random.randint(0, arrange)
             if index < arrange:
-                temp = word_list[index]
-                word_list[index] = word_list[index + 1]
-                word_list[index + 1] = temp
-            else:
-                temp = word_list[index]
-                word_list[index] = word_list[index - index]
-                word_list[index - index] = temp
+                temp = words[index]
+                words[index] = words[index - 1]
+                words[index - 1] = temp
         arrange-=1
-    words = ' '.join(word_list)
+    words = ' '.join(words)
     print(words)
 
 if __name__ == "__main__":
     params = sys.argv[1:]
-    word_1 = params[0]
-    word_2 = params[1]
-    word_3 = params[2]
-    word_4 = params[3]
-
-    rearrange(word_1, word_2, word_3, word_4)
+    words = []
+    for index in range(len(params)):
+        word = params[index]
+        words.append(word)
+    rearrange(words)
