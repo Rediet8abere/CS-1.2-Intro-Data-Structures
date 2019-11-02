bb06aefcce01867232d85e0d2227a3c4ddf16373
Rediet8abere <redietaberegesesse@gmail.com>
2019-11-01 21:38:38 -0700

    port changed

--- Code/app.py
+++ Code/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from dictionary_words import *
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     # print(random_words())
     return random_words(10)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
