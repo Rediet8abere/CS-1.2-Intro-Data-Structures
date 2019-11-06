bb1738a67a99aedbb887fa7eb8a9dab9f53647b0
Rediet8abere <redietaberegesesse@gmail.com>
2019-11-06 15:44:24 -0800

    adding dict and list

--- Code/app.py
+++ Code/app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from dictionary_words import *
 import os
-
-app = Flask(__name__)
+# virtulal enviroment
+app = Flask(__name__, static_url_path='',
+            static_folder='templates/static')
 # I think you should create a histogram with a source_text here instead of using the histogram of random_words
 @app.route('/')
 def hello_world():
     # print(random_words())
-    return random_words(10)
+    return render_template('index.html', sentence=random_words(10))
+    # return , words=random_words(10))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
